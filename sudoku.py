ab085c4a05a351e20a29410045ff51b2d734bc6e
SMMousaviSP <smmousavisp@gmail.com>
2019-11-05 22:13:26 +0330

    Add check_box method

--- sudoku.py
+++ sudoku.py
@@ -8,7 +8,7 @@ sudokuCorrect = [[8, 1, 2, 7, 5, 3, 6, 4, 9],
                  [4, 3, 8, 5, 2, 6, 9, 1, 7],
                  [7, 9, 6, 3, 1, 8, 4, 5, 2]]
 
-# first row and first box have to 9
+# First row and first box have two 9
 sudokuWrong   = [[8, 9, 2, 7, 5, 3, 6, 4, 9],
                  [9, 4, 3, 6, 8, 2, 1, 7, 5],
                  [6, 7, 5, 4, 9, 1, 2, 8, 3],
@@ -25,6 +25,18 @@ def check_row(s, row):
         if flag[i-1]:
             return False
         flag[i-1] = True
+    for i in flag:
+        if not i:
+            return False
+    return True
+
+def check_box(s, row, col):
+    flag = [False for i in range(9)]
+    for i in range(row, row + 3):
+        for j in range(col, col + 3):
+            if flag[s[i][j]-1]:
+                return False
+            flag[s[i][j]-1] = True
     for i in flag:
         if not i:
             return False
