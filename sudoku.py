9824ed398a3641d7cd90a3aea96ef59f4967c4f1
SMMousaviSP <smmousavisp@gmail.com>
2019-11-06 20:06:18 +0330

    Add printing status to all methods

--- sudoku.py
+++ sudoku.py
@@ -26,38 +26,56 @@ def check_flag(flag):
     return True
 
 def check_row(s, row):
+    print("Start check_row method with row " + str(row))
     flag = [False for i in range(9)]
     for i in s[row]:
         if flag[i-1]:
+            print("End check_row method with return value False")
             return False
         flag[i-1] = True
-    return check_flag(flag)
+    final_flag = check_flag(flag)
+    print("End check_row method with return value " + str(final_flag))
+    return final_flag
 
 def check_col(s, col):
+    print("Start check_col method with col " + str(col))
     flag = [False for i in range(9)]
     for i in range(9):
         if flag[s[i][col]-1]:
+            print("End check_col method with return value False")
             return False
         flag[s[i][col]-1] = True
-    return check_flag(flag)
+    final_flag = check_flag(flag)
+    print("End check_col method with return value " + str(final_flag))
+    return final_flag
 
 def check_box(s, row, col):
+    print("Start check_box method with row " + str(row) + " and col " + str(col))
     flag = [False for i in range(9)]
     for i in range(row, row + 3):
         for j in range(col, col + 3):
             if flag[s[i][j]-1]:
+                print("End check_box method with return value False")
                 return False
             flag[s[i][j]-1] = True
-    return check_flag(flag)
+    final_flag = check_flag(flag)
+    print("End check_box method with return value " + str(final_flag))
+    return final_flag
 
 def check_all_row(s):
+    print("Start check_all_row method")
     for i in range(9):
         if not check_row(s, i):
+            print("End check_all_row method with return value False")
             return False
+    print("End check_all_row method with return value True")
     return True
 
 def check_all_col(s):
+    print("Start check_all_col method")
     for i in range(9):
         if not check_col(s, i):
+            print("End check_all_col method with return value False")
             return False
+    print("End check_all_col method with return value True")
     return True
